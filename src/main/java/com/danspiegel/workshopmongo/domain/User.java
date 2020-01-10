6c7d4d56357857cfad3fae25752736df65d5e9e7
Daniel Spiegel <danispiegel@gmail.com>
2020-01-10 18:02:00 -0300

    Fianlizado projeto de Spring Boot, Spring Data e MongoDB

--- src/main/java/com/danspiegel/workshopmongo/domain/User.java
+++ src/main/java/com/danspiegel/workshopmongo/domain/User.java
@@ -1,20 +1,26 @@
 package com.danspiegel.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection="user")
 public class User implements Serializable {
-
+	private static final long serialVersionUID = 1L;
 	
-	private static final long serialVersionUID = 6716951430380175520L;
-	
-	//--> Atributos
-	
+	@Id
 	private String id;
 	private String name;
 	private String email;
 	
+	@DBRef(lazy = true)
+	private List<Post> posts = new ArrayList<>();
+	
 	public User() {
-		
 	}
 
 	public User(String id, String name, String email) {
@@ -24,8 +30,6 @@ public class User implements Serializable {
 		this.email = email;
 	}
 
-	//--> Getters e Setters
-	
 	public String getId() {
 		return id;
 	}
@@ -48,6 +52,14 @@ public class User implements Serializable {
 
 	public void setEmail(String email) {
 		this.email = email;
+	}
+
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
 	}
 
 	@Override
@@ -74,5 +86,4 @@ public class User implements Serializable {
 			return false;
 		return true;
 	}
-	
 }
---
+++ src/main/java/com/danspiegel/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.danspiegel.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.danspiegel.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
